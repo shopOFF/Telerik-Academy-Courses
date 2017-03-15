38f676afbc10ccc1b13eedd09b688b2119d5434e
shopov91 <shopov91@gmail.com>
2017-03-15 17:40:43 +0200

    Updated web controls hw

---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-and-HTML-Controls/HW/HW/TelerikAcademyHWWebFormsWebHTMLControls/RandomNumberGenerator.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RandomNumberGenerator.aspx.cs" Inherits="TelerikAcademyHWWebFormsWebHTMLControls.RandomNumberGenerator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input id="Text1" type="text" runat="server" />
+            <asp:Literal Text=" - Enter Range From" runat="server" ID="FromLiteral" />
+            <br />
+            <input id="Text2" type="text" runat="server" />
+            <asp:Literal Text=" - Enter Range To" runat="server" ID="ToLiteral" />
+            <br />
+            <br />
+            <input id="Button1" type="button" value="Generate Random Number" onserverclick="Button1_Click" runat="server" />
+            <br />
+            <br />
+            <asp:Literal Text="Generated Number Is: " runat="server" />
+            <asp:Literal ID="GeneratedNumberLiteral" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-and-HTML-Controls/HW/HW/TelerikAcademyHWWebFormsWebHTMLControls/RandomNumberGenerator.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikAcademyHWWebFormsWebHTMLControls
+{
+    public partial class RandomNumberGenerator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var fromNumber = int.Parse(this.Text1.Value);
+            var toNumber = int.Parse(this.Text2.Value);
+
+            var rndNumGenerator = new RndNumGenerator();
+            var rndNum = rndNumGenerator.GenerateRandomNumber(fromNumber, toNumber);
+
+            this.GeneratedNumberLiteral.Text = rndNum.ToString();
+        }
+
+        private class RndNumGenerator
+        {
+            public int GenerateRandomNumber(int fromNum, int toNum)
+            {
+                var generator = new Random();
+                
+                return generator.Next(fromNum, toNum);
+            }
+        }
+    }
+}
