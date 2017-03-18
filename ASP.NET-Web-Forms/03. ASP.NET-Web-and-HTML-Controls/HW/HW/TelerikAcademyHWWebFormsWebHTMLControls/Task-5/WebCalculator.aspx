183dcbaf18e153898fb73a3b9c18cf76a251de9f
shopov91 <shopov91@gmail.com>
2017-03-18 18:21:09 +0200

    Task 5 implemented one way

---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-and-HTML-Controls/HW/HW/TelerikAcademyHWWebFormsWebHTMLControls/Task-5/WebCalculator.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebCalculator.aspx.cs" Inherits="TelerikAcademyHWWebFormsWebHTMLControls.Task_5.WebCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Web Calculator</title>
+    <link href="Styles/CalculatorStyles.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div id="calculator-container">
+                <asp:TextBox ID="TextBoxOutput" runat="server"></asp:TextBox>
+                <br />
+                <div id="buttons-container">
+                    <asp:Button ID="Button1" runat="server" Text="1" OnClick="Button1_Click" />
+                    <asp:Button ID="Button2" runat="server" Text="2" OnClick="Button2_Click" />
+                    <asp:Button ID="Button3" runat="server" Text="3" OnClick="Button3_Click" />
+                    <asp:Button ID="ButtonPlus" runat="server" Text="+" OnClick="ButtonPlus_Click" />
+                    <br />
+                    <asp:Button ID="Button4" runat="server" Text="4" OnClick="Button4_Click" />
+                    <asp:Button ID="Button5" runat="server" Text="5" OnClick="Button5_Click" />
+                    <asp:Button ID="Button6" runat="server" Text="6" OnClick="Button6_Click" />
+                    <asp:Button ID="ButtonMinus" runat="server" Text="-" OnClick="ButtonMinus_Click" />
+                    <br />
+                    <asp:Button ID="Button7" runat="server" Text="7" OnClick="Button7_Click" />
+                    <asp:Button ID="Button8" runat="server" Text="8" OnClick="Button8_Click" />
+                    <asp:Button ID="Button9" runat="server" Text="9" OnClick="Button9_Click" />
+                    <asp:Button ID="ButtonMultiply" runat="server" Text="x" OnClick="ButtonMultiply_Click" />
+                    <br />
+                    <asp:Button ID="ButtonClear" runat="server" Text="Clear" OnClick="ButtonClear_Click" />
+                    <asp:Button ID="Button0" runat="server" Text="0" OnClick="Button0_Click" />
+                    <asp:Button ID="ButtonDivis" runat="server" Text="/" OnClick="ButtonDivis_Click" />
+                    <asp:Button ID="ButtonSquareRoot" runat="server" Text="√" OnClick="ButtonSquareRoot_Click" />
+                    <br />
+                    <asp:Button ID="ButtonEquals" runat="server" Text="=" OnClick="ButtonEquals_Click" />
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-and-HTML-Controls/HW/HW/TelerikAcademyHWWebFormsWebHTMLControls/Task-5/WebCalculator.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikAcademyHWWebFormsWebHTMLControls.Task_5
+{
+    public partial class WebCalculator : System.Web.UI.Page
+    {
+        private const string SQUARE_ROOT = " √ ";
+        private const string MINUS = " - ";
+        private const string PLUS = " + ";
+        private const string DIVISE = " / ";
+        private const string MULTIPLY = " x ";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "1";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "2";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "3";
+        }
+
+        protected void ButtonPlus_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += PLUS;
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "4";
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "5";
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "6";
+        }
+
+        protected void ButtonMinus_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += MINUS;
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "7";
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "8";
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "9";
+        }
+
+        protected void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += MULTIPLY;
+        }
+
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text = string.Empty;
+        }
+
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += "0";
+        }
+
+        protected void ButtonDivis_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += DIVISE;
+        }
+
+        protected void ButtonSquareRoot_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text += SQUARE_ROOT;
+        }
+
+        protected void ButtonEquals_Click(object sender, EventArgs e)
+        {
+            this.TextBoxOutput.Text = ResultCalculator(this.TextBoxOutput.Text);
+        }
+
+        private string ResultCalculator(string equation)
+        {
+            var splitEquation = equation.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (splitEquation.Contains("√") && splitEquation.Count == 2)
+            {
+                return Math.Sqrt(long.Parse(splitEquation[0])).ToString();
+            }
+            else if (splitEquation.Count > 1)
+            {
+                double result = long.Parse(splitEquation[0]);
+
+                for (int i = 1; i < splitEquation.Count - 1; i++)
+                {
+                    if (splitEquation[i] == "+")
+                    {
+                        result += long.Parse(splitEquation[i + 1]);
+                    }
+                    else if (splitEquation[i] == "-")
+                    {
+                        result -= long.Parse(splitEquation[i + 1]);
+                    }
+                    else if (splitEquation[i] == "√")
+                    {
+                        result = Math.Sqrt(long.Parse(splitEquation[i - 1]));
+                    }
+                    else if (splitEquation[i] == "/")
+                    {
+                        result /= long.Parse(splitEquation[i + 1]);
+                    }
+                    else if (splitEquation[i] == "x")
+                    {
+                        result *= long.Parse(splitEquation[i + 1]);
+                    }
+                }
+
+                return result.ToString();
+            }
+            else
+            {
+                return this.TextBoxOutput.Text;
+            }
+        }
+    }
+}
