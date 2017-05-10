f06e8103e2cfec2380aa0b67b2e2caa5e0bb19c3
shopov91 <shopov91@gmail.com>
2017-05-10 12:12:08 +0300

    File upload hw partiali implemented

---
+++ ASP.NET-Web-Forms/07. ASP.NET-File-Upload/Homework/WebFormsFileUpHW/WebFormsFileUpHW/FileUpTest.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpTest.aspx.cs" Inherits="WebFormsFileUpHW.FileUpTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUploadTest" runat="server" />
+        <br />
+        <asp:Button Text="Upload" runat="server" OnClick="UploadBtn_Click" />
+        <br />
+        <asp:Label ID="Label_Result" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Web-Forms/07. ASP.NET-File-Upload/Homework/WebFormsFileUpHW/WebFormsFileUpHW/FileUpTest.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsFileUpHW
+{
+    public partial class FileUpTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadBtn_Click(object sender, EventArgs e)
+        {
+            if (this.FileUploadTest.HasFile)
+            {
+                string fileName = Path.GetFileName(this.FileUploadTest.FileName);
+                this.FileUploadTest.SaveAs(Server.MapPath($"~/Uploaded_Files/{fileName}"));
+                this.Label_Result.Text= "Upload status: File uploaded!";
+            }
+        }
+    }
+}
