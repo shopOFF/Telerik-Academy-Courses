a0e27ed0b6abea38db04ada3553b13a81ad387c9
shopoff <shopov91@gmail.com>
2017-11-28 12:12:10 +0200

    mvc templates added

---
+++ ASP.NET-MVC/ASP.NET-MVC-Templates/ASP.NET Core/Data/AspNetCoreTemplate.Data/TemporaryDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace AspNetCoreTemplate.Data
+{
+    using System.IO;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+
+    // TODO: Remove. (part of the workaround for https://github.com/aspnet/EntityFramework/issues/5320)
+    public class TemporaryDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
